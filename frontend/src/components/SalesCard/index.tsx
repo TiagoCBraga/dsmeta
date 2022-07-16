338d964842682d91cd19221a8a2fbbe4d8ce638c
Tiago C Braga <tiago.rck258@gmail.com>
2022-07-16 00:00:50 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 
 import NotificationButton from '../NotificationButton';
 import './styles.css';
@@ -14,11 +16,13 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
-        .then( response => (
-            console.log(response.data)
-        ))
+        axios.get(`${BASE_URL}/sales`)
+            .then(response => (
+                setSales(response.data.content)
+            ));
     }, []);
 
     return (
@@ -55,50 +59,32 @@ function SalesCard() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="dsmeta-data-apartir-width-992px">#341</td>
-                                <td className="dsmeta-data-apartir-width-576px">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="dsmeta-data-apartir-width-992px">15</td>
-                                <td className="dsmeta-data-apartir-width-992px">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="dsmeta-data-apartir-width-992px">#341</td>
-                                <td className="dsmeta-data-apartir-width-576px">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="dsmeta-data-apartir-width-992px">15</td>
-                                <td className="dsmeta-data-apartir-width-992px">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="dsmeta-data-apartir-width-992px">#341</td>
-                                <td className="dsmeta-data-apartir-width-576px">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="dsmeta-data-apartir-width-992px">15</td>
-                                <td className="dsmeta-data-apartir-width-992px">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
+                            {sales.map(sale => {
+                                return (
+
+                                    < tr key={sale.id}>
+                                        <td className="dsmeta-data-apartir-width-992px">{sale.id}</td>
+                                        <td className="dsmeta-data-apartir-width-576px">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="dsmeta-data-apartir-width-992px">{sale.visited}</td>
+                                        <td className="dsmeta-data-apartir-width-992px">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+
+                            }
+
                         </tbody>
                     </table>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
